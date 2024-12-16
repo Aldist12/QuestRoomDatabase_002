353ab23f8166f178c3f8169e6f88df5db5baceb4
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-16 23:38:32 +0700

    Mengimplementasikan alur StateFlow untuk homeUiState

--- app/src/main/java/com/example/praktikum7/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum7/ui/viewmodel/HomeMhsViewModel.kt
@@ -2,7 +2,38 @@ package com.example.praktikum7.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.praktikum7.repository.RepositoryMhs
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 
 class HomeMhsViewModel(
     private val repositoryMhs: RepositoryMhs
 ) : ViewModel()
+val homeUiState: StateFlow<HomeUiState> = RepositoryMhs.getAllMhs()
+    .filterNotNull()
+    .map {
+        HomeUiState(
+            listMhs = it.toList(),
+            isLoading = false,
+        )
+    }
+    .onStart {
+        emit(HomeUiState(isLoading = true))
+        delay(900)
+    }
+    .catch {
+        emit(
+            HomeUiState(
+                isLoading = false,
+                isError = true,
+                errorMessage = it.message ?: "Terjadi kesalahan"
+            )
+        )
+    }
+    .stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = HomeUiState(
+            isLoading = true,
+        )
+    )
+
