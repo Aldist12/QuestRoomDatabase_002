b0834925d460cbe1ed658735cb725501b8998c79
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-16 23:27:48 +0700

    Membuat data class DetailUiState untuk menyimpan status UI

--- app/src/main/java/com/example/praktikum7/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum7/ui/viewmodel/DetailMhsViewModel.kt
@@ -46,4 +46,17 @@ fun deleteMhs() {
         }
     }
 }
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
 
