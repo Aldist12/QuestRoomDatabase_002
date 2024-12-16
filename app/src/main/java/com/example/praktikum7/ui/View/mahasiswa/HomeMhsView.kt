1421b191175811f4101bf429ef2704e9a8a39a08
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-17 03:36:46 +0700

    Menambahkan fungsi HomeMhsView untuk menampilkan daftar mahasiswa dan tombol tambah mahasiswa

---
+++ app/src/main/java/com/example/praktikum7/ui/View/mahasiswa/HomeMhsView.kt
@@ -0,0 +1,55 @@
+package com.example.praktikum7.ui.View.mahasiswa
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.praktikum7.ui.costumwidget.TopAppBar
+import com.example.praktikum7.ui.viewmodel.HomeMhsViewModel
+import com.example.praktikum7.ui.viewmodel.PenyediaViewModel
+import java.lang.reflect.Modifier
+
+@Composable
+fun HomeMhsView(
+    viewModel: HomeMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddMhs: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                onBack = { },
+                showBackButton = false,
+                judul = "Daftar Mahasiswa"
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddMhs,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Mahasiswa"
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeUiState by viewModel.homeUiState.collectAsState()
+
+        BodyHomeMhsView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
