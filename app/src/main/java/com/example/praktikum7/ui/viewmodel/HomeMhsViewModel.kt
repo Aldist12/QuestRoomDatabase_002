fcd0a7a57ebc4aeebcf3a7f13e5bc1db8d560f72
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-16 23:41:31 +0700

    Membuat data class HomeUiState untuk menyimpan status UI

--- app/src/main/java/com/example/praktikum7/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum7/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.praktikum7.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.praktikum7.data.entity.Mahasiswa
 import com.example.praktikum7.repository.RepositoryMhs
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.internal.NopCollector.emit
 
 class HomeMhsViewModel(
     private val repositoryMhs: RepositoryMhs
@@ -36,4 +39,11 @@ val homeUiState: StateFlow<HomeUiState> = RepositoryMhs.getAllMhs()
             isLoading = true,
         )
     )
+data class HomeUiState(
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
+
 
