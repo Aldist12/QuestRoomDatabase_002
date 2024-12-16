de957b73879e3209d04d255b83154d659e8741df
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-16 23:24:39 +0700

    Mengimplementasikan alur StateFlow untuk detailUiEvent

--- app/src/main/java/com/example/praktikum7/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum7/ui/viewmodel/DetailMhsViewModel.kt
@@ -3,6 +3,7 @@ package com.example.praktikum7.ui.viewmodel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.praktikum7.repository.RepositoryMhs
+import kotlinx.coroutines.flow.StateFlow
 
 class DetailMhsViewModel(
     savedStateHandle: SavedStateHandle,
@@ -10,3 +11,31 @@ class DetailMhsViewModel(
 ) : ViewModel() {
     private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
 }
+val detailUiEvent: StateFlow<DetailUiState> = RepositoryMhs.getMhs(_nim)
+    .filterNotNull()
+    .map {
+        DetailUiState(
+            detailUiEvent = it.toDetailUiEvent(),
+            isLoading = false,
+        )
+    }
+    .onStart {
+        emit(DetailUiState(isLoading = true))
+        delay(600)
+    }
+    .catch {
+        emit(
+            DetailUiState(
+                isLoading = false,
+                isError = true,
+                errorMessage = it.message ?: "Terjadi Kesalahan",
+            )
+        )
+    }
+    .stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(2000),
+        initialValue = DetailUiState(
+            isLoading = true,
+        )
+    )
