3ea79938ff447fe1eeacd5642568350be2cca20c
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-17 03:39:30 +0700

    Menambahkan fungsi BodyHomeMhsView untuk menangani status loading, error, dan menampilkan daftar mahasiswa

--- app/src/main/java/com/example/praktikum7/ui/View/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/praktikum7/ui/View/mahasiswa/HomeMhsView.kt
@@ -1,16 +1,28 @@
 package com.example.praktikum7.ui.View.mahasiswa
 
+import android.text.Layout.Alignment
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum7.ui.costumwidget.TopAppBar
 import com.example.praktikum7.ui.viewmodel.HomeMhsViewModel
+import com.example.praktikum7.ui.viewmodel.HomeUiState
 import com.example.praktikum7.ui.viewmodel.PenyediaViewModel
 import java.lang.reflect.Modifier
 
@@ -52,4 +64,60 @@ fun HomeMhsView(
             modifier = Modifier.padding(innerPadding)
         )
     }
+}
+    @Composable
+    fun BodyHomeMhsView(
+        homeUiState: HomeUiState,
+        onClick: (String) -> Unit = { },
+        modifier: Modifier = Modifier
+    ) {
+        val coroutineScope = rememberCoroutineScope()
+        val snackbarHostState = remember { SnackbarHostState() }
+        when {
+            homeUiState.isLoading -> {
+                Box(
+                    modifier = modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+
+            homeUiState.isError -> {
+                LaunchedEffect(homeUiState.errorMessage) {
+                    homeUiState.errorMessage?.let { message ->
+                        coroutineScope.launch {
+                            snackbarHostState.showSnackbar(message)
+                        }
+                    }
+                }
+            }
+
+            homeUiState.listMhs.isEmpty() -> {
+                Box(
+                    modifier = modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Tidak ada data mahasiswa.",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
+
+            else -> {
+                ListMahasiswa(
+                    listMhs = homeUiState.listMhs,
+                    onCLick = {
+                        onClick(it)
+                    },
+                    modifier = Modifier.padding(top = 100.dp)
+                )
+            }
+        }
+    }
+
 }
