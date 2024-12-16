da145de47385ba7768d75b3017584e4e7d373ab6
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-17 04:19:47 +0700

    Fix Error Code

--- app/src/main/java/com/example/praktikum7/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum7/ui/viewmodel/DetailMhsViewModel.kt
@@ -2,51 +2,64 @@ package com.example.praktikum7.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.praktikum7.data.entity.Mahasiswa
 import com.example.praktikum7.repository.RepositoryMhs
+import com.example.praktikum7.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class DetailMhsViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs,
 ) : ViewModel() {
     private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
-}
-val detailUiEvent: StateFlow<DetailUiState> = RepositoryMhs.getMhs(_nim)
-    .filterNotNull()
-    .map {
-        DetailUiState(
-            detailUiEvent = it.toDetailUiEvent(),
-            isLoading = false,
-        )
-    }
-    .onStart {
-        emit(DetailUiState(isLoading = true))
-        delay(600)
-    }
-    .catch {
-        emit(
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
             DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
                 isLoading = false,
-                isError = true,
-                errorMessage = it.message ?: "Terjadi Kesalahan",
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
             )
         )
-    }
-    .stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(2000),
-        initialValue = DetailUiState(
-            isLoading = true,
-        )
-    )
-fun deleteMhs() {
-    detailUiEvent.value.detailUiEvent.toMahasiswaEntity().let {
-        viewModelScope.launch {
-            RepositoryMhs.deleteMhs(it)
+
+    fun deleteMhs() {
+        detailUiState.value.detailUiEvent.toMahasiswaEntity().let {
+            viewModelScope.launch {
+                repositoryMhs.deleteMhs(it)
+            }
         }
     }
 }
+
 data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
     val isLoading: Boolean = false,
@@ -59,6 +72,7 @@ data class DetailUiState(
     val isUiEventNotEmpty: Boolean
         get() = detailUiEvent != MahasiswaEvent()
 }
+
 fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent {
     return MahasiswaEvent(
         nim = nim,
@@ -69,5 +83,3 @@ fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent {
         angkatan = angkatan
     )
 }
-
-
